d955a4b113140877396bdf45fde0ad59b818126b
Yevhenii Orenchuk <orenchuk98@gmail.com>
2017-11-28 23:20:35 +0200

    added getter and setter for cart in CartDecorator

--- src/main/java/CartDecorator.java
+++ src/main/java/CartDecorator.java
@@ -4,6 +4,14 @@ public abstract class CartDecorator {
 
     private Cart cart;
 
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
     public CartDecorator(Cart cart) {
         this.cart = cart;
     }
